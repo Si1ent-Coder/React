cef24497e073d8a3ab186bf83f7520f19a0eda7a
Raghav Chauhan <1raghav.chauhan@gmail.com>
2026-06-16 01:01:53 +0530

    update dsa

---
+++ Video61/plantrip/src/App.js
@@ -0,0 +1,33 @@
+import logo from './logo.svg';
+import './App.css';
+import data from './data';
+import { useState } from 'react';
+
+import Tours from './components/Tours';
+
+function App() {
+
+    const[tours,setTours] = useState(data);
+
+    function removeTour(id){
+        const newTours = tours.filter(tours => tours.id !==id);
+        setTours(newTours);
+    }
+    if(tours.length==0){
+      return(
+        <div className='refresh'>
+          <h1>No Tours Left</h1>
+          <button className='btn-white'
+          onClick={()=>setTours(data)}>Refresh</button>
+        </div>
+      )
+    }
+
+  return (
+    <div className='App'>
+      <Tours tours={tours} removeTour={removeTour}></Tours>
+    </div>
+  );
+}
+
+export default App;
