f27e08572894eb5e79a1d075d1a795f9d8418df5
Raghav Chauhan <1raghav.chauhan@gmail.com>
2026-07-05 22:06:57 +0530

    Updates dev

---
+++ Video63/Courses/src/App.jsx
@@ -0,0 +1,23 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+import './App.css'
+import Navbar from './Components/Navbar.jsx';
+
+
+const App = () =>{
+  
+  return(
+    <div>
+
+      <Navbar/>      
+
+
+    </div>
+  )
+
+}
+
+
+export default App;
---
+++ Video63/Courses/src/Components/Navbar.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+
+const Navbar = () =>{
+    return(
+        <div>
+            <h2>Top Courses</h2>
+        </div>
+    )
+}
+
+export default Navbar;
